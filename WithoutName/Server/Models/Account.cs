1003e6b55430d58fa3926e9afed03aee6a806bc0
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2021-08-04 18:25:34 +0300

    feat: add models on srv

---
+++ WithoutName/Server/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutName.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+
+        public int CurrencyId { get; set; }
+        public virtual Currency Currency { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
