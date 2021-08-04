3a2a6a7456149860d715c7b4544ac09373b832d3
ZerAtul7 <manekrafter@gmail.com>
2021-08-04 18:16:44 +0300

    feat: init 3 classes
    Category,Currency,Transaction

--- WithoutName/WithoutName/Models/Transaction.cs
+++ WithoutName/WithoutName/Models/Transaction.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WithoutName.Models
 {
-    class Transaction
+    public class Transaction
     {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public int CurrencyId { get; set; }
+        public virtual Currency Currency { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
