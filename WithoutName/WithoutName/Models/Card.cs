22c4bd826bd454ad67378c64d1199066044b4cdd
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2021-08-04 18:23:00 +0300

    feat: init classes

--- WithoutName/WithoutName/Models/Card.cs
+++ WithoutName/WithoutName/Models/Card.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WithoutName.Models;
 
 namespace WithoutName.Models
 {
-    class Card
+    public class Card
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
     }
 }
