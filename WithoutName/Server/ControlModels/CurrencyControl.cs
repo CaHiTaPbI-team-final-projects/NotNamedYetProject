ec76f5b37d8b7b31e0886bce8ef96c30a879f325
Danya Boisan <boberiosif@gmail.com>
2021-08-11 00:54:54 +0300

    feat: Rewrite classes

--- WithoutName/Server/ControlModels/CurrencyControl.cs
+++ WithoutName/Server/ControlModels/CurrencyControl.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Server.EF;
+using Server.Models;
 namespace Server.ControlModels
 {
     class CurrencyControl
     {
+        private EDM edm;
+        public void SetEDM(EDM edm)
+        {
+            this.edm = edm;
+        }
         public void ADD_CURENCY()
         { }
 
--- WithoutName/Server/ControlModels/TransactionControl.cs
+++ WithoutName/Server/ControlModels/TransactionControl.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Server.EF;
+using Server.Models;
 namespace Server.ControlModels
 {
     class TransactionControl
     {
+        private EDM edm;
+        public void SetEDM(EDM edm)
+        {
+            this.edm = edm;
+        }
         public void ADD_TRANSACTION(decimal sum, int categoryId, int currencyId)
         {
             //dolbaza.Transactions.Add(new Transaction() { Amount = sum, CategoryId = categoryId, CurrencyId = currencyId, UserId = 0 });
