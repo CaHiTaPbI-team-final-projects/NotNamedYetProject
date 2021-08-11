f4c4bbda538bb7bfdf0ab3ac2edb13bd78204c6d
Danya Boisan <boberiosif@gmail.com>
2021-08-11 23:46:59 +0300

    feat: add code to controls

---
+++ WithoutName/Server/Models/Params.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    [Serializable]
+    public class Params
+    {
+        public object ClassForSend { get; set; }
+        public string Command { get; set; }
+    }
+}
