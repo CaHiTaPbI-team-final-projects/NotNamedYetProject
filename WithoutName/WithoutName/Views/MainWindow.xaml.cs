32781b41e62895d3f129dcabc715cd0a8da5b7bd
ZerAtul7 <manekrafter@gmail.com>
2021-08-11 18:42:58 +0300

    fix: MainWidowViewModel

--- WithoutName/WithoutName/Views/MainWindow.xaml.cs
+++ WithoutName/WithoutName/Views/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WithoutName.ViewModels;
-
+using WithoutName.Models;
 
 namespace WithoutName.Views
 {
